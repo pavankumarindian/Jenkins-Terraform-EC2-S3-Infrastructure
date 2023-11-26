ad2cb6c264831603e09d95dd42a02eb8fd7cfac3
Pavan Kumar Gudipati <142888833+pavankumarindian@users.noreply.github.com>
2023-11-26 19:44:31 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "pavanssonixbucket1"
+    bucket         = "my-s3bucket-for-tf-state"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
